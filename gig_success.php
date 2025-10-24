6cf5b1beb53989154538db4ddbc3e51f6801bc3b
LawranceAckshayan <lackshayan123@gmail.com>
2025-10-25 01:54:18 +0530

    part of G13

---
+++ gig_success.php
@@ -0,0 +1,20 @@
+<?php
+include 'config.php';
+include 'headers.php';
+?>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Success</title>
+</head>
+<body>
+<div class="container">
+  <div class="card">
+    <h2>✅ Gig Updated successfully</h2>
+    <p>Your gig has been updated successfully. Thank you!</p>
+    <p><a href="owner_dashboard.php" class="btn">Back to Dashboard</a></p>
+  </div>
+</div>
+</body>
+</html>
